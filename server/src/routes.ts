1ebd904d1b106c8bc7c505a55ccf48c84fb200b2
HallanCosta <hallan.costa1@hotmail.com>
2020-07-06 21:02:17 -0300

    Adding cors in server

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,15 +3,12 @@ import knex from './database/connection';
 
 import CarsController from './controllers/CarsController';
 
-
 const routes = express.Router();
 
 const carsController = new CarsController; 
 
 routes.get('/cars', carsController.index);
-
 routes.post('/cars', carsController.create);
-
 routes.delete('/cars/:id', carsController.delete);
 
 export default routes;
