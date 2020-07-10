05282ae4f9afa99f411b1466b5bdf4ed06442117
HallanCosta <hallan.costa1@hotmail.com>
2020-07-10 19:44:38 -0300

    Solving offset pagination problem

--- server/src/controllers/CarsController.ts
+++ server/src/controllers/CarsController.ts
@@ -6,12 +6,17 @@ class CarsController {
   async index(request: Request, response: Response) {
     const { page = 1 } = request.query;
     
+    const [count] = await knex('cars').count();
+
     const cars = await knex('cars')
       .select('*')
-      .offset((page) - 1)
+      .offset(((page) - 1) * 5)
       .limit(5);
 
-    return response.json(cars);
+    return response.json({
+      cars, 
+      count: count['count(*)']
+    });
   }
 
   async create(request : Request, response: Response) {
